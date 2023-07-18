52cb16dfc58b72260c80f1fae2ad3c082a74c60d
parth bhatnagar <parthbhatnagar@parths-MacBook-Pro.local>
2023-07-18 12:09:21 +0530

    Added toggle for hide and show

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ let toolsContainer = document.querySelector(".tools-container");
 let pencilContainer = document.querySelector(".pencil-tool-container");
 let eraserToolContainer = document.querySelector(".eraser-tool-container");
 let icon = optionsContainer.children[0];
+let pencilIcon = document.querySelector(".pencil");
+let eraserIcon = document.querySelector(".eraser");
+let showPencil = false;
+let showEraser = false;
 
 const onClick = (event) => {
   showOptions = !showOptions;
-
   if (showOptions) {
     openTools();
   } else {
     closeTools();
+  }
+};
+
+const pencilCLick = () => {
+  showPencil = !showPencil;
+  if (showPencil) {
+    pencilContainer.style.display = "block";
+  } else {
+    pencilContainer.style.display = "none";
+  }
+};
+
+const eraserClick = () => {
+  showEraser = !showEraser;
+  if (showEraser) {
+    eraserToolContainer.style.display = "flex";
+  } else {
+    eraserToolContainer.style.display = "none";
   }
 };
 
@@ -30,3 +51,5 @@ const closeTools = () => {
 };
 
 optionsContainer.addEventListener("click", onClick);
+pencilIcon.addEventListener("click", pencilCLick);
+eraserIcon.addEventListener("click", eraserClick);
