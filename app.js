284d7babac70fed12e265053037246680a376b10
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-09-30 08:15:05 +0530

    Cors issues

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require("express"); // Access express server
 const socket = require("socket.io");
 const cors = require("cors");
 const app = express(); // Initialised and server ready
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.static("public"));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 const port = 3000;
 
 // listens and returns a server
